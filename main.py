5a5b32b7176b4563e72612ea53a3710bdb93a5b0
Nick Rothwell <nick@cassiel.com>
2016-12-26 10:40:35 +0000

    Copy from client project.

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+from flask_cors import CORS, cross_origin
+
+LOCAL = False
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+CORS(app)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@socketio.on('my event')
+def handle_my_custom_event(json):
+    print('received json: ' + str(json))
+    emit('to client', json, broadcast=True)
+
+if LOCAL and __name__ == "__main__":
+    socketio.run(app, debug=False)
+
+# To deploy:
+# gunicorn main:socketio
